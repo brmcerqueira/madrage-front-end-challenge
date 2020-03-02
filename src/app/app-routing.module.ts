dea5d67a39fb09d54075eff20564ea5e6d3869ec
brmcerqueira <bruno.cerqueira@fieldcorp.com.br>
2020-03-02 18:40:37 -0300

    Toolbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,14 +6,6 @@ const routes: Routes = [
     path: 'home',
     loadChildren: () => import('./feed/feed.module').then(m => m.FeedModule)
   },
-  {
-    path: 'direct',
-    loadChildren: () => import('./direct/direct.module').then(m => m.DirectModule)
-  },
-  {
-    path: 'shopping',
-    loadChildren: () => import('./shopping/shopping.module').then(m => m.ShoppingModule)
-  },
   { path: '**', redirectTo: 'home' }
 ];
 
