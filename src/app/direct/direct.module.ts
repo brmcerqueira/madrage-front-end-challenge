71eab291e4c0cda1c7c8701a26d4d689c35440bd
brmcerqueira <bruno.cerqueira@fieldcorp.com.br>
2020-03-03 00:28:00 -0300

    Direct

--- src/app/direct/direct.module.ts
+++ src/app/direct/direct.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { DirectComponent } from './direct.component';
+import { MaterialModule } from '../material.module';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
     declarations: [DirectComponent],
     imports: [
         CommonModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        MaterialModule,
+        TranslateModule
     ]
 })
 export class DirectModule { }
