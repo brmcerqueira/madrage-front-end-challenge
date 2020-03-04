8ae7329cdd3860b3df1659e7d5305aa93b84eec6
brmcerqueira <bruno.cerqueira@fieldcorp.com.br>
2020-03-04 01:59:10 -0300

    Ajuste Direct

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ViewContainerRef, ComponentFactoryResolver, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'madrage-front-end-challenge';
+export class AppComponent implements OnInit {
+
+  @ViewChild('directContainer', { read: ViewContainerRef })
+  private directViewContainerRef: ViewContainerRef;
+
+  @ViewChild('shoppingContainer', { read: ViewContainerRef })
+  private shoppingViewContainerRef: ViewContainerRef;
+
+  constructor(private readonly componentFactoryResolver: ComponentFactoryResolver) { 
+
+  }
+
+  ngOnInit(): void {
+    import('./direct/direct.module')
+    .then(() => import('./direct/direct.component'))
+    .then(({ DirectComponent }) => {
+      this.directViewContainerRef.createComponent(
+        this.componentFactoryResolver.resolveComponentFactory(DirectComponent));
+    });
+    import('./shopping/shopping.module')
+    .then(() => import('./shopping/shopping.component'))
+    .then(({ ShoppingComponent }) => {
+      this.shoppingViewContainerRef.createComponent(
+        this.componentFactoryResolver.resolveComponentFactory(ShoppingComponent));
+    });
+  }
 }
--- src/app/feed/feed.component.ts
+++ src/app/feed/feed.component.ts
@@ -6,29 +6,7 @@ import { Component, OnInit, ViewChild, ViewContainerRef, ComponentFactoryResolve
   styleUrls: ['./feed.component.scss']
 })
 export class FeedComponent implements OnInit {
-
-  @ViewChild('directContainer', { read: ViewContainerRef })
-  private directViewContainerRef: ViewContainerRef;
-
-  @ViewChild('shoppingContainer', { read: ViewContainerRef })
-  private shoppingViewContainerRef: ViewContainerRef;
-
-  constructor(private readonly componentFactoryResolver: ComponentFactoryResolver) { 
-
-  }
-
   ngOnInit(): void {
-    import('../direct/direct.module')
-    .then(() => import('../direct/direct.component'))
-    .then(({ DirectComponent }) => {
-      this.directViewContainerRef.createComponent(
-        this.componentFactoryResolver.resolveComponentFactory(DirectComponent));
-    });
-    import('../shopping/shopping.module')
-    .then(() => import('../shopping/shopping.component'))
-    .then(({ ShoppingComponent }) => {
-      this.shoppingViewContainerRef.createComponent(
-        this.componentFactoryResolver.resolveComponentFactory(ShoppingComponent));
-    });
+
   }
 }
