d977a17d6827729a56d8d04f9526215442f08843
brmcerqueira <bruno.cerqueira@fieldcorp.com.br>
2020-03-04 02:34:48 -0300

    Ajuste Feed

--- src/app/feed/feed.module.ts
+++ src/app/feed/feed.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
 import { FeedComponent } from './feed.component';
 import { MaterialModule } from '../material.module';
+import { TranslateModule } from '@ngx-translate/core';
 
 const routes: Routes = [
     {
@@ -17,7 +18,9 @@ const routes: Routes = [
     imports: [
         RouterModule.forChild(routes),
         CommonModule,
+        FormsModule,
         ReactiveFormsModule,
+        TranslateModule,
         MaterialModule
     ]
 })
