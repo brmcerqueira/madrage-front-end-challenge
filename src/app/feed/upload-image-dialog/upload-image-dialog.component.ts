42f9aab3f041433fe828078d3c078e405081d86c
brmcerqueira <bruno.cerqueira@fieldcorp.com.br>
2020-03-05 16:25:03 -0300

    ImageCropped

--- src/app/feed/upload-image-dialog/upload-image-dialog.component.ts
+++ src/app/feed/upload-image-dialog/upload-image-dialog.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
+import { ImageCroppedEvent } from 'ngx-image-cropper';
 
 @Component({
   selector: 'app-upload-image-dialog',
@@ -8,11 +9,34 @@ import { MatDialogRef } from '@angular/material/dialog';
 })
 export class UploadImageDialogComponent {
 
+  imageChangedEvent: any = '';
+  croppedImage: any = '';
+
   constructor(private dialogRef: MatDialogRef<UploadImageDialogComponent>) {
 
   }
 
-  onNoClick(): void {
+  fileChangeEvent(event: any): void {
+      this.imageChangedEvent = event;
+  }
+
+  imageCropped(event: ImageCroppedEvent) {
+      this.croppedImage = event.base64;
+  }
+
+  imageLoaded() {
+      // show cropper
+  }
+
+  cropperReady() {
+      // cropper ready
+  }
+
+  loadImageFailed() {
+      // show message
+  }
+
+  close(): void {
     this.dialogRef.close();
   }
 }
