17dff53d0e60c3b1f5299915046460d7204e752c
brmcerqueira <bruno.cerqueira@fieldcorp.com.br>
2020-03-06 01:20:05 -0300

    Shopping

--- src/app/shopping/shopping.module.ts
+++ src/app/shopping/shopping.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ShoppingComponent } from './shopping.component';
+import { MaterialModule } from '../material.module';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
     declarations: [ShoppingComponent],
     imports: [
         CommonModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        MaterialModule,
+        TranslateModule
     ]
 })
 export class ShoppingModule { }
